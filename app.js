fc40a09d997d15ee24e72162e15678511e7837fe
salomon.escobar <salomon.escobar@company-corp.com>
2020-04-01 12:32:33 -0500

    added index.html, app.js and css files

---
+++ app.js
@@ -0,0 +1,26 @@
+const Pet = ({ name, animal, breed }) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {} , name),
+        React.createElement("h2", {} , animal),
+        React.createElement("h2", {} , breed)
+    ]);
+};
+
+const App = () => {
+    return React.createElement("div", { id: "something-not-important" }, [
+          React.createElement("h1", {}, "Adopt Me!"),
+          React.createElement(Pet, {
+              name: "Luna",
+              animal: "Dog",
+              breed: "Monster"
+          }),
+          React.createElement(Pet, {
+              name: "Jagger",
+              animal: "Dog",
+              breed: "Shepard"
+          }),
+          React.createElement(Pet, { name: "Turbo", animal: "Cat", breed: "Mixed" })
+    ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
